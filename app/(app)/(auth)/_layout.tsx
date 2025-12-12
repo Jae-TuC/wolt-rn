3225dde9c4e6994b06f74b3662654cad30245182
Justice Anyaogu <jaetuc21@gmail.com>
2025-12-12 02:25:24 +0100

    imlement scroll base header change animation

--- app/(app)/(auth)/_layout.tsx
+++ app/(app)/(auth)/_layout.tsx
@@ -1,9 +1,44 @@
-import { Stack } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import { Stack, useRouter } from 'expo-router'
+import { TouchableOpacity } from 'react-native'
 
 const Layout = () => {
+    const router = useRouter()
     return (
         <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="(modal)/location" options={{
+                presentation: "formSheet",
+                sheetAllowedDetents: [0.7],
+                title: "",
+                headerShadowVisible: false,
+                headerShown: false,
+                sheetCornerRadius: 16,
+                sheetGrabberVisible: true,
+                headerRight: () => (
+                    <TouchableOpacity style={{ padding: 4, borderRadius: 20 }} onPress={() => router.dismiss()}>
+                        <Ionicons name='close-sharp' size={28} color='#666' />
+                    </TouchableOpacity>
+                ),
+            }} />
+            <Stack.Screen name="(modal)/filter" options={{
+                presentation: "formSheet",
+                sheetAllowedDetents: [0.8],
+                title: "",
+                headerShown: false,
+                headerShadowVisible: false,
+                sheetCornerRadius: 16,
+                contentStyle: {
+                    backgroundColor: "#fff",
+                    overflow: "hidden",
+                },
+                sheetGrabberVisible: true,
+                headerRight: () => (
+                    <TouchableOpacity style={{ padding: 4, borderRadius: 20 }} onPress={() => router.dismiss()}>
+                        <Ionicons name='close-sharp' size={28} color='#666' />
+                    </TouchableOpacity>
+                ),
+            }} />
         </Stack>
     )
 }
--- components/screens/RestuarantListPage.tsx
+++ components/screens/RestuarantListPage.tsx
@@ -1,15 +1,26 @@
 import { Fonts } from '@/constants/theme'
 import { StyleSheet, Text, View } from 'react-native'
-import Animated from 'react-native-reanimated'
+import Animated, { useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { CategoryList } from '../CategoryList'
+import RestaurantHeader from '../RestaurantHeader'
 import RestuarantList from '../RestuarantList'
 
 const RestuarantListPage = () => {
     const insets = useSafeAreaInsets()
+    const scrollOffset = useSharedValue(0)
+
+    const scrollHander = useAnimatedScrollHandler({
+        onScroll: (event) => {
+            scrollOffset.value = event.contentOffset.y
+        }
+    })
+
     return (
         <View style={styles.container}>
+            <RestaurantHeader title="Restuarant" scrollOffset={scrollOffset} />
             <Animated.ScrollView
+                onScroll={scrollHander}
                 showsVerticalScrollIndicator={false}
                 contentContainerStyle={{ paddingTop: insets.top + 60 }}
             >
